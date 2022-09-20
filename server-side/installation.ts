5980c2e407ae4a56e0f18ccb86c31c7b4c22def6
Saar Guttman <saar.g@pepperi.com>
2022-09-20 11:34:10 +0300

    installation: Create and remove dimx relations

--- server-side/installation.ts
+++ server-side/installation.ts
@@ -9,7 +9,7 @@ The error Message is importent! it will be written in the audit log and help the
 */
 
 import { Client, Request } from '@pepperi-addons/debug-server'
-import { AddonDataScheme, PapiClient } from '@pepperi-addons/papi-sdk'
+import { AddonDataScheme, PapiClient, Relation } from '@pepperi-addons/papi-sdk'
 import { SCHEMA_NAME } from 'surveys-shared';
 
 
@@ -17,7 +17,8 @@ export async function install(client: Client, request: Request): Promise<any>
 {
     
 	const papiClient = createPapiClient(client);
-	createSurveySchema(papiClient, client);
+	await createSurveySchema(papiClient, client);
+	await createDimxRelations(client, papiClient);
 	return {success:true,resultObject:{}}
 }
 
@@ -25,7 +26,7 @@ export async function uninstall(client: Client, request: Request): Promise<any>
 {
 
 	const papiClient = createPapiClient(client);
-	// No need to purge the schema on uninstall...
+	await removeDimxRelations(client, papiClient);
 	return {success:true,resultObject:{}}
 }
 
@@ -104,7 +105,46 @@ async function createSurveySchema(papiClient: PapiClient, client: Client)
 	await papiClient.addons.data.schemes.post(schema);
 }
 
-async function purgeSurveySchema(papiClient: PapiClient)
+async function createDimxRelations(client: Client, papiClient: PapiClient)
 {
-	await papiClient.post('/addons/data/schemes/survey/purge');
+	const isHidden = false;
+	await postDimxRelations(client, isHidden, papiClient);
+}
+
+async function removeDimxRelations(client: Client, papiClient: PapiClient)
+{
+	const isHidden = true;
+	await postDimxRelations(client, isHidden, papiClient);
+}
+
+async function postDimxRelations(client: Client, isHidden: boolean, papiClient: PapiClient) 
+{
+
+		const importRelation: Relation = {
+			RelationName: "DataImportResource",
+			AddonUUID: client.AddonUUID,
+			AddonRelativeURL: '',
+			Name: SCHEMA_NAME,
+			Type: 'AddonAPI',
+			Source: 'adal',
+			Hidden: isHidden
+		};
+
+		const exportRelation: Relation = {
+			RelationName: "DataExportResource",
+			AddonUUID: client.AddonUUID,
+			AddonRelativeURL: '',
+			Name: SCHEMA_NAME,
+			Type: 'AddonAPI',
+			Source: 'adal',
+			Hidden: isHidden
+		};
+
+		await upsertRelation(papiClient, importRelation);
+		await upsertRelation(papiClient, exportRelation);
+}
+
+async function upsertRelation(papiClient: PapiClient, relation: Relation) 
+{
+	return papiClient.post('/addons/data/relations', relation);
 }
